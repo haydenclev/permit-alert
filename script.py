ce9a67125a112184897b4edf7897ff0294560446
Hayden <hayden.clev@gmail.com>
2025-08-06 22:41:36 -0700

    refactor: add type checking

--- script.py
+++ script.py
@@ -6,21 +6,23 @@ import smtplib
 from email.message import EmailMessage
 from pydantic import BaseModel
 
-config = {}
-
 
 class Config(BaseModel):
     api_url: str
     web_url: str
     start_date: str
     state_file: str
-    default_state: object
+    default_state: dict
     email_alerts: bool
     email_from: str
     email_to: str
 
 
-def check_for_updates(url):
+with open("config.yaml") as stream:
+    config = Config(**yaml.safe_load(stream))
+
+
+def check_for_updates(url) -> tuple:
     try:
         headers = {
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36",
@@ -51,7 +53,7 @@ def check_for_updates(url):
     return (permits, updates)
 
 
-def read_permit_state():
+def read_permit_state() -> dict:
     if os.path.exists(config.state_file):
         print(f"File '{config.state_file}' found. Loading data...")
         try:
@@ -67,7 +69,7 @@ def read_permit_state():
     return config.default_state
 
 
-def write_permit_state(permits):
+def write_permit_state(permits) -> None:
     print(f"Writing updated data to '{config.state_file}'...")
     try:
         with open(config.state_file, "w") as f:
@@ -77,7 +79,7 @@ def write_permit_state(permits):
         print(f"An error occurred while writing to the file: {e}")
 
 
-def send_email_alert(permits):
+def send_email_alert(permits) -> None:
     msg = EmailMessage()
     msg["Subject"] = "🎒 Permit Available on Recreation.gov"
     msg["From"] = config.email_from
@@ -91,7 +93,7 @@ def send_email_alert(permits):
 
     try:
         with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp:
-            smtp.login(config.email_from, os.environ.get("EMAIL_PWD"))
+            smtp.login(config.email_from, os.environ.get("EMAIL_PWD") or "")
             smtp.send_message(msg)
         print("Alert email sent.")
     except Exception as e:
@@ -99,8 +101,6 @@ def send_email_alert(permits):
 
 
 if __name__ == "__main__":
-    with open("config.yaml") as stream:
-        config = Config(**yaml.safe_load(stream))
     (permits, changes) = check_for_updates(config.api_url)
     if changes:
         print("Permits available:", permits)
